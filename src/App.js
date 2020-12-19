a54fe0d44959bf7aba1ff91210f0521bc712deb7
Ji Eun Han <hanji0514@ajou.ac.kr>
2020-12-20 03:14:28 +0900

    installation and editor component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  editorRef = React.createRef();
+
+  handleClick = () => {
+    this.editorRef.current.getInstance().exec('Bold');
+  };
+
+  render() {
+    return (
+      <>
+        <Editor
+          previewStyle="vertical"
+          height="400px"
+          initialEditType="markdown"
+          initialValue="hello"
+          ref={this.editorRef}
+        />
+        <button onClick={this.handleClick}>make bold</button>
+      </>
+    );
+  }
 }
-
 export default App;
