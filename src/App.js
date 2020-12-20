251e67fc6ee59541ed575d78c07f4fbdd3626940
Ji Eun Han <hanji0514@ajou.ac.kr>
2020-12-20 17:07:17 +0900

    using editor state and plugin installation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import React, {Component} from 'react';
 import 'codemirror/lib/codemirror.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
+//color plugin
+import 'tui-color-picker/dist/tui-color-picker.css';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+//chart plugin
+import 'tui-chart/dist/tui-chart.css';
+import chart from '@toast-ui/editor-plugin-chart';
 
 class App extends Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      content :''
+    }
+  } 
+ // handleClick = () => {
+  //   this.editorRef.current.getInstance().exec('Bold');
+  // };
   editorRef = React.createRef();
-
-  handleClick = () => {
-    this.editorRef.current.getInstance().exec('Bold');
-  };
-
+  handleChangeEditor = e =>
+  {
+    const content = this.editorRef.current.getInstance().getMarkdown();
+    this.setState({
+      content:content
+    })
+  }
+  
+  
   render() {
     return (
       <>
@@ -19,8 +40,20 @@ class App extends Component {
           initialEditType="markdown"
           initialValue="hello"
           ref={this.editorRef}
+          plugins= {[colorSyntax, chart]}
         />
-        <button onClick={this.handleClick}>make bold</button>
+        <div id="toastUIEditor">
+          <h1>Toast UI Editor Example</h1>
+          <div id="button">
+            <button className="btn_save" onClick={this.handleChangeEditor}>Save</button>
+          </div>
+        <div>
+          <h2>Result</h2>
+          <textarea className="result" value={this.state.content} readOnly="readOnly"></textarea>
+        </div>
+        </div>
+        
+        {/* <button onClick={this.handleClick}>make bold</button> */}
       </>
     );
   }
